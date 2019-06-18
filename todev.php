76366668a2dc4b2682b7bfb34396fecebacda88b
Abiel MaGu <abielmagu@gmail.com>
2019-06-18 17:28:05 -0500

    Update todev.php

--- todev.php
+++ todev.php
@@ -5,4 +5,6 @@
 - Nonces security
 - About, FAQ, etc... plugin
 - Delete folders and images of posts publicity
+- Need Jquery and set on file-js: $('#popub-link').trigger('click')
+- Copy and paste this where is will execute: if( function_exists('popub_showtime') ) popub_showtime();
 */
